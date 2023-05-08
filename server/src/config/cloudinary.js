faf548ad48bd8d941e759cc11006721ce2e07127
thinzarhninyu <thinzarhninyu.17@gmail.com>
2023-05-08 10:30:59 +0800

    did cloudinary.js for upload and get

--- server/src/config/cloudinary.js
+++ server/src/config/cloudinary.js
@@ -1,9 +1,57 @@
 const config = require("./config");
 const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
+const chalk = require("chalk");
 
 cloudinary.config({
   cloud_name: config.cloudinary_cloud_name,
   api_key: config.cloudinary_api_key,
   api_secret: config.cloudinary_api_secret,
 });
+
+module.exports.destroyFromCloudinary = (publicID) => {
+  console.log(chalk.yellow(publicID));
+  console.log(chalk.blue("destroyFromCloudinary called"));
+  return new Promise(async (resolve, reject) => {
+    console.log(chalk.blue("async running"));
+    const result = await cloudinary.uploader.destroy(publicID);
+    console.log(chalk.yellow(result));
+    if (result.result === "not found") {
+      return reject({ status: "fail", data: result });
+    } else {
+      return resolve({ status: "success", data: result });
+    }
+  });
+};
+
+module.exports.uploadStreamToCloudinary = (buffer) => {
+  return new Promise((resolve, reject) => {
+    console.log(chalk.yellow(buffer));
+    console.log(chalk.blue("uploadStreamToCloudinary called"));
+    const streamDestination = cloudinary.uploader.upload_stream(
+      {
+        folder: "ades",
+        allowed_formats: "png,jpg",
+        resource_types: "image",
+      },
+      async (error, result) => {
+        console.log(chalk.blue("async running"));
+        if (result) {
+          console.log(chalk.yellow(result));
+          const cloudinaryFileData = {
+            url: result.url,
+            publicID: result.public_id,
+            status: "success",
+          };
+          console.log(chalk.green("success cloudinary"));
+          return resolve({ status: "success", data: cloudinaryFileData });
+        }
+        if (error) {
+          console.error(chalk.red(error));
+          reject({ status: "fail", data: error });
+        }
+      }
+    );
+    streamifier.createReadStream(buffer).pipe(streamDestination);
+  });
+};
