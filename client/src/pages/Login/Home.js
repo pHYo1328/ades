56e3b704a2124bb0853fa12a2482ddba8ab0906a
KohHanMing123 <kohhanming123@gmail.com>
2023-05-09 16:40:15 +0800

    login frontend done

--- client/src/pages/Login/Home.js
+++ client/src/pages/Login/Home.js
@@ -1,20 +1,24 @@
+// import { Link } from "react-router-dom";
+import React from 'react'
 function Home() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content text-center">
-              <div className="max-w-md">
-                <h1 className="text-5xl font-bold mb-5">Home page</h1>
-  
-                <button className="btn btn-secondary">Sign out</button>
+  return (
+    <div className="App">
+      <header className="">
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 class="text-5xl font-bold mb-5">Home page</h1>
+              <div class="flex items-center justify-end mb-5">
+                <button class="bg-pink-800 text-white font-bold py-2 px-4 rounded-md mr-3">Sign out</button>
+                <a href="/login" class="bg-gray-400 text-white font-bold py-2 px-4 rounded-md text-lg">Login</a>
+                <a href="/register" class="bg-gray-200 text-gray-800 font-bold py-2 px-4 rounded-md text-lg ml-3">Register</a>
               </div>
             </div>
           </div>
-        </header>
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
