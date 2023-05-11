1ab2fe707c4fa6c6a5dbdb2f3610da0c81201d8c
KohHanMing123 <kohhanming123@gmail.com>
2023-05-11 15:28:24 +0800

    register finished, login half done

--- server/server.js
+++ server/server.js
@@ -1,8 +1,65 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./src/routes/routes');
+const path = require('path');
+const corsOptions = require('../server/src/config/corsOptions');
+const { logger } = require('../server/src/middlewares/logEvents');
+const errorHandler = require('../server/src/middlewares/errorHandler');
+const verifyJWT = require('../server/src/middlewares/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('../server/src/middlewares/credentials');
+
+
+// custom middleware logger
+app.use(logger);
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+//middleware for cookies
+app.use(cookieParser());
+
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+// routes
+
+app.use('/', require('../server/src/routes/root'));
+app.use('/register', require('../server/src/routes/register'));
+app.use('/auth', require('../server/src/routes/auth'));
+// app.use('/refresh', require('../server/src/routes/refresh'));
+// app.use('/logout', require('../server/src/routes/logout'));
+
+app.use(verifyJWT);
+// app.use('/employees', require('../server/src/routes/api/employees'));
+// app.use('/users', require('../server/src/routes/api/users'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+app.use(errorHandler);
+
 app.use(bodyParser.json());
 app.use(
   cors({
@@ -12,8 +69,10 @@ app.use(
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
+
 const router = express.Router();
-app.use(router);
+
+app.use(router);  
 
 // Centralized temporary error handling
 app.use((err, req, res, next) => {
@@ -31,5 +90,5 @@ routes(app, router);
 const port = process.env.PORT || 8081;
 app.listen(port, function () {
   // eslint-disable-next-line no-console
-  console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`); 
 });
---
+++ server/src/controller/registerController.js
@@ -0,0 +1,25 @@
+const loginServices = require("../services/login.services");
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        return res.status(400).json({ 'message': 'All fields are required.' });
+    }
+
+    try {
+        // encrypt the password
+        const hashedPwd = await bcrypt.hash(password, 10);
+
+        // register the new user
+        const result = await loginServices.registerUser(username, email, hashedPwd);
+
+        console.log(result);
+
+        res.status(201).json({ 'success': `New user ${username} created!` });
+    } catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
+};
+
+module.exports = { handleNewUser };
