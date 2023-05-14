ecd92c0943620b41b6138b4fa3c1f02ea56ba8ea
thinzarhninyu <thinzarhninyu.17@gmail.com>
2023-05-14 15:10:38 +0800

    cloudinary done

---
+++ client/src/components/upload_widget.js
@@ -0,0 +1,25 @@
+import cloudinary from "cloudinary-core";
+const cloudName = "ddoajstil"; // replace with your own cloud name
+const uploadPreset = "q7grvgxu"; // replace with your own upload preset
+const myWidget = cloudinary.createUploadWidget(
+  {
+    cloudName: cloudName,
+    uploadPreset: uploadPreset,
+  },
+  (error, result) => {
+    if (!error && result && result.event === "success") {
+      console.log("Done! Here is the image info: ", result.info);
+      document
+        .getElementById("uploadedimage")
+        .setAttribute("src", result.info.secure_url);
+    }
+  }
+);
+
+document.getElementById("upload_widget").addEventListener(
+  "click",
+  function () {
+    myWidget.open();
+  },
+  false
+);
