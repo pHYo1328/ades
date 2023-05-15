81c34021441116d8e4ccf212827a5b6671074928
KohHanMing123 <kohhanming123@gmail.com>
2023-05-15 22:29:29 +0800

    login with jwt and refresh done

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -3,7 +3,10 @@ const cartController = require('../controller/cart.controller');
 const orderController = require('../controller/order.controller');
 const paymentController = require('../controller/payment.controller');
 const checkoutController = require('../controller/checkout.controller')
-//const verifyAccessToken = require("../middlewares/verifyAccessToken");
+const registerController = require('../controller/registerController');
+const authController = require('../controller/authController');
+const refreshTokenController = require('../controller/refreshTokenController');
+// const verifyAccessToken = require("../middlewares/verifyAccessToken");
 
 module.exports = (app, router) => {
   router.get(
@@ -143,4 +146,21 @@ module.exports = (app, router) => {
   "/create-payment-intent", 
   checkoutController.createPaymentIntent
   );
+  
+  router.get(
+  '^/$|/index(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+  });
+  
+  router.post(
+  '/register', registerController.handleNewUser
+  );
+
+  router.post(
+  '/login', authController.handleLogin
+  );
+
+  router.get(
+    '/refresh', refreshTokenController.handleRefreshToken
+    );
 };
