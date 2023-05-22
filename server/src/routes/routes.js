7f01675ed8e636cd97517d9e03ece1d50d310b6c
thinzarhninyu <thinzarhninyu.17@gmail.com>
2023-05-22 11:43:48 +0800

    all features done (mostly) minor changes + frontend left to fix

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -96,10 +96,6 @@ module.exports = (app, router) => {
     '/api/products/inventory/minus/:productID',
     productController.processUpdateInventoryDown
   );
-  router.put(
-    '/api/products/:productID/images',
-    productController.processDeleteProductImages
-  );
 
   // PHYO
 
@@ -204,6 +200,9 @@ module.exports = (app, router) => {
   router.post('/login-admin', authAdminController.handleLogin);
   router.get('/refresh-admin', refreshTokenAdminController.handleRefreshToken);
   router.put('/logout-admin', logoutAdminController.handleLogout);
-  router.put('/forgot-admin', forgotPasswordAdminController.handleForgotPassword);
+  router.put(
+    '/forgot-admin',
+    forgotPasswordAdminController.handleForgotPassword
+  );
   router.post('/verify-otp-admin', verifyOTPAdminController.verifyOTP);
 };
