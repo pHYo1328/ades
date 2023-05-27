3b69ea08f550f2e85914475f8c6cf3befe9781c2
pHYo <86188804+pHYo1328@users.noreply.github.com>
2023-05-27 15:04:54 +0800

    header changes

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -44,27 +44,36 @@ const Header = () => {
                 User
               </button>
               {isUserPanelOpen && (
-                <div className="absolute top-10 right-0 bg-white text-gray-800 border border-gray-300 rounded-md py-2 shadow-lg">
+                <div className="z-10 absolute top-10 right-0 bg-white text-gray-800 border border-gray-300 rounded-md py-2 shadow-lg">
+                  <div className='flex flex-row'>
+                  <Link to="/orderToPay">
                   <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
                     <FaWallet className="inline-block mr-2" />
                     to pay
                   </button>
+                  </Link>
+                  <Link to="/orderToShip">
                   <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
                     <FaBox className="inline-block mr-2" />
                     to ship
                   </button>
+                  </Link>
+                    </div>
+                  <div className='flex flex-row'>
+                  <Link to="/orderToDeliver">
                   <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
                     <RiTruckLine className="inline-block mr-2" />
                     to receive
                   </button>
+                  </Link>
+                  <Link to="/orderDelivered">
                   <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
                     <RiCheckboxCircleLine className="inline-block mr-2" />
                     completed
                   </button>
-                  <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
-                    <RiCloseCircleLine className="inline-block mr-2" />
-                    cancelled
-                  </button>
+                  </Link>
+                    </div>
+
                 </div>
               )}
             </div>
