731e851b3eafd78aacf62654f1bf5da1d433993d
KohHanMing123 <kohhanming123@gmail.com>
2023-05-27 14:53:54 +0800

    added z index

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import {
   RiTruckLine,
@@ -10,9 +10,25 @@ import { FaBox, FaWallet } from 'react-icons/fa';
 const Header = () => {
   const [isUserPanelOpen, setIsUserPanelOpen] = useState(false);
   const userId = localStorage.getItem('userid');
+  const userPanelRef = useRef(null);
+  
   const handleUserPanelToggle = () => {
     setIsUserPanelOpen(!isUserPanelOpen);
   };
+
+  const handleOutsideClick = (event) => {
+    if (userPanelRef.current && !userPanelRef.current.contains(event.target)) {
+      setIsUserPanelOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
 
   return (
     <header className="bg-white shadow">
@@ -36,7 +52,7 @@ const Header = () => {
             <Link to="/category" className="text-gray-800 hover:text-gray-600">
               Category
             </Link>
-            <div className="relative">
+            <div className="relative" ref={userPanelRef}>
               <button
                 onClick={handleUserPanelToggle}
                 className="text-gray-800 hover:text-gray-600"
@@ -44,7 +60,8 @@ const Header = () => {
                 User
               </button>
               {isUserPanelOpen && (
-                <div className="absolute top-10 right-0 bg-white text-gray-800 border border-gray-300 rounded-md py-2 shadow-lg">
+                <div className="absolute top-10 right-0 bg-white text-gray-800 border border-gray-300 rounded-md py-2 shadow-lg z-10">
+                  {/* User panel content */}
                   <button className="block w-full text-left px-4 py-2 hover:bg-gray-100">
                     <FaWallet className="inline-block mr-2" />
                     to pay
@@ -68,6 +85,7 @@ const Header = () => {
                 </div>
               )}
             </div>
+
             <Link to="/login">
               <button className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-md text-lg">
                 Sign In
--- client/src/pages/Login/Home.js
+++ client/src/pages/Login/Home.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className="bg-gray-100">
-     <main>
+      <main>
         <div className="container mx-auto px-4 py-10">
           <h1 className="text-5xl font-bold mb-5">Home page</h1>
-          {/* Add your content here */}
+          <div>button to be removed</div>
+          <Link to="/login-admin">
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md text-lg">
+              Go to Admin Login
+            </button>
+          </Link>
         </div>
       </main>
     </div>
