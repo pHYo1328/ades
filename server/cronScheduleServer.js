9e7c10bcfc2cefb9d2740d7d48fb85fa3a0cb473
Kaung Si Phyo <235756ksp@gmail.com>
2023-05-29 00:58:41 +0800

    minor changes

--- server/cronScheduleServer.js
+++ server/cronScheduleServer.js
@@ -1,9 +1,12 @@
 const bookmarkEmailServices = require('./src/services/bookmarkEmail.services');
 const cron = require('node-cron');
-console.log("auto server started");
-// cron.schedule('*/5 * * * * *', bookmarkEmailServices.updateProductsEmailSender); //for testing purposes
-cron.schedule('0 */6 * * *', () => {
-  bookmarkEmailServices.updateProductsEmailSender().catch((error) => {
-    console.error('Error in updateProductsEmailSender:', error);
+const app = express();
+
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000`);
+  cron.schedule('0 */6 * * *', () => {
+    bookmarkEmailServices.updateProductsEmailSender().catch((error) => {
+      console.error('Error in updateProductsEmailSender:', error);
+    });
   });
 });
