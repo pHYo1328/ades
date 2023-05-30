f8ec4993912acf82a707e7c45c4b234ffd1f1802
pHYo <86188804+pHYo1328@users.noreply.github.com>
2023-05-31 02:59:43 +0800

    Revert "Merge branch 'main' of https://github.com/pHYo1328/assignment-ay2324s1-classs-2b21-team-3"
    This reverts commit d0a66956dd17aac5651ffb421888bfefa0818ef2, reversing
    changes made to f86c403c9f06c16cff4c8de6e474c0fc682cf0eb.

--- client/src/pages/Products/Public/ProductsPage.js
+++ client/src/pages/Products/Public/ProductsPage.js
@@ -228,7 +228,7 @@ export default function ProductsPage() {
           </div>
         </div>
 
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:gap-x-8 h-100">
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:gap-x-8">
           {products ? (
             products.map((product) => (
               <div key={product.product_id} className="group relative">
