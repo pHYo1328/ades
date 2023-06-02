33998c8cb25f33bba9a292de4a8321f478aaa6c2
KohHanMing123 <kohhanming123@gmail.com>
2023-06-02 17:13:53 +0800

    comments for otp email, logout and userInfo

--- server/src/services/loginOTPEmail.js
+++ server/src/services/loginOTPEmail.js
@@ -2,6 +2,7 @@ const chalk = require('chalk');
 const pool = require('../config/database');
 const sendInBlue = require('../config/sendinblue');
 
+// to generate random OTP
 function generateOTP() {
   var otp = Math.floor(100000 + Math.random() * 900000);
   return otp;
@@ -9,31 +10,31 @@ function generateOTP() {
 
 module.exports.OTPEmailSender = async (username) => {
   console.log(chalk.blue('Cron schedule Started'));
-
+  // query to check if username exists
   const userQuery = 'SELECT username, email FROM users WHERE username = ?';
 
   try {
     console.log('im inside try this is my unsername', username);
     const result = await pool.query(userQuery, [username]);
     console.log('Result:', result[0]);
-    const rows = result[0]; // Access the rows returned from the query
+    const rows = result[0]; // Access the rows returned from the query 
     console.log('Rows:', rows);
     if (rows.length > 0) {
-      // Process each row
+      // Process each row 
       const response = await Promise.all(
         rows.map(async (row) => {
           const users = row;
           const username = users.username;
           const email = users.email;
-          const otp = generateOTP(); // Generate OTP
+          const otp = generateOTP(); // Generate random OTP 
 
-          // Save the OTP in the database for the user
+          // Save the OTP in the database for the user 
           await pool.query('UPDATE users SET otp = ? WHERE username = ?', [
             otp,
             username,
           ]);
 
-          // Send the OTP email using Sendinblue
+          // Send the OTP email using Sendinblue 
           const data = await sendInBlue.sendTransacEmail({
             subject: 'Hello from Our TechZero',
             sender: { email: 'techZero@gmail.com', name: 'techZero' },
