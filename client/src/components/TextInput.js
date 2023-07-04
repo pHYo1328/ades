639412996a8271ef341b13276ee5df643f7da9d6
Kaung Si Phyo <235756ksp@gmail.com>
2023-07-05 04:08:38 +0800

    cart page changing style

--- client/src/components/TextInput.js
+++ client/src/components/TextInput.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default function TextInput({
   placeholder,
   name,
@@ -6,6 +5,7 @@ export default function TextInput({
   value,
   func,
   buttonRef,
+  defaultValue = null
 }) {
   return (
     <input
@@ -25,24 +25,4 @@ export default function TextInput({
     />
   );
 }
-=======
-export default function TextInput({ placeholder, value, func, buttonRef, defaultValue = null }) {
-    return (
-        <input
-            type="text"
-            className="border border-gray-300 rounded-md py-2 px-3 w-full"
-            placeholder={placeholder}
-            defaultValue={defaultValue}
-            value={value}
-            onChange={func}
-            onKeyDown={buttonRef && ((event) => {
-                if (event.key === 'Enter') {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    buttonRef.current.click();
-                }
-            })}
-        />
-    )
-}
->>>>>>> 299adb2c39a96eca140de8b078796cb9aabdd773
+
